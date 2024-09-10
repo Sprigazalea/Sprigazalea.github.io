2291331505c66a7c10370b948eeee4efcfa60315
Riley <dickersonevan82@gmail.com>
2024-09-10 15:16:22 -0400

    added js script that isnt working yet for dropdown functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 const threeBarsButton = document.getElementById("dropdown-button")
+const dropdownMenu = document.getElementById("dropdown")
 
-threeBarsButton.addEventListener("click", )
+function displayDropdown() {
+    if (dropdownMenu.style.display === "none") {
+        dropdownMenu.style.display = "flex";
+    } else if (dropdownMenu.style.display === "flex") {
+        dropdownMenu.style.display = "none"
+    }
+}
+
+threeBarsButton.addEventListener("click", displayDropdown())
